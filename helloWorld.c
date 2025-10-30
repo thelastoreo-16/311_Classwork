938635167e451070518d7151fcbf1dbb243344a0
thelastoreo-16 <priya164@outlook.com>
2025-10-30 12:18:05 -0400

    updated hello world

--- helloWorld.c
+++ helloWorld.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    printf("Hello Dr. Kidd. My name is Rachel Pallipamula-Niranjeen and I'm in your CMPE311 class.\n");
     return 0;
 }
